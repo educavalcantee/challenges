74ab2ab3f6888eb9dd12bd93ab17ca66c9d2c617
Edu Cavalcante <educavalcantee@gmail.com>
2023-05-08 23:28:45 -0300

    adicionado texto da tarefa com os conceitos pedidos

--- OlaGit.playground/Pages/Comentarios.xcplaygroundpage/Contents.swift
+++ OlaGit.playground/Pages/Comentarios.xcplaygroundpage/Contents.swift
@@ -1,3 +1,26 @@
 /*Estou aprendendo a mexer no Git para a aula OlaGit
 Este será o arquivo que enviarei para o meu repositório!*/
 
+/*
+ Git: é um sistema de controle de versionamento. Registra quaisquer alterações feitas em um código, e permite acessar versões anteriores caso desejado, economizando tempo e recursos.
+ 
+ Github: Plataforma online para o gerenciamento de códigos e ambiente de colaboração entre devs. Nele é possível criar repositórios, e neles, hospedar nossos projetos.
+ 
+ GItFlow: Modelo estratégico de fluxo de trabalho que auxilia na organização do versionamento de códigos. Tem por objetivo melhorar a organizaçao das branches dentro dos repositórios de forma a dar mais fluidez ao processo.
+ 
+ Comandos Git:
+ 
+ git init - cria um novo repositório ou reinicia um já existente.
+ 
+ git add - adiciona um arquivo alterado para a stage area, isto [e, o prepara para ser vinculado a um commit.
+ 
+ git commit - move os arquivos da stage area para o repositorio local (salva as alterações).
+ 
+ git branch - cria uma nova branch no projeto.
+ 
+ git checkout - troca entre uma versáo e outra do projeto (alterna entre as branches).
+ 
+ git pull - traz um arquivo do repositorio remoto para o local
+ 
+ git push - envia arquivos do repositorio local para o remoto.
+ */
